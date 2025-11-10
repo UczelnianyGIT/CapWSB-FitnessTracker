a4bef0fb8f5fd5cc89cb91f4d9465c20e7daf728
UczelnianyGIT <wrx100896@student.wroclaw.merito.pl>
2025-11-10 18:58:01 +0100

    Krok 3

--- src/main/java/pl/wsb/fitnesstracker/FitnessTracker.java
+++ src/main/java/pl/wsb/fitnesstracker/FitnessTracker.java
@@ -1,3 +1,5 @@
+package pl.wsb.fitnesstracker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
